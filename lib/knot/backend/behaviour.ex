35a20376d62d265c1090dbc22334dc47fa54192e
Stefan <zivkovicstefan011@gmail.com>
2024-12-07 21:38:35 +0100

    Defines the behavior for backends

--- lib/knot/backend/behaviour.ex
+++ lib/knot/backend/behaviour.ex
@@ -1,2 +1,9 @@
 defmodule Knot.Backend.Behaviour do
+  @moduledoc """
+  Defines the behavior for backends used by Knot.
+  """
+
+  @callback get_state(context_id :: any) :: {map(), integer()}
+  @callback set_state(context_id :: any, state :: map(), version :: integer()) :: :ok
+  @callback get_state_history(context_id :: any, client_version :: integer()) :: list(map())
 end
